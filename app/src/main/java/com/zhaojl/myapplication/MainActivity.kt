f9d78857ac37ce48e35a6ba0822c607c3971a710
Jianling Zhao <zhaojianling@yinxiang.com>
2022-02-16 10:12:00 +0800

    init

---
+++ app/src/main/java/com/zhaojl/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.zhaojl.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zhaojl.myapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/zhaojl/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.zhaojl.myapplication.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
